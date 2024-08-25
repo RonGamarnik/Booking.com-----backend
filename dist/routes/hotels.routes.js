0a287f48a7a91416d5b9020982d181bbcfea75b2
RonGamarnik <rgam2002@gmail.com>
2024-08-25 12:36:32 +0300

    Aivan Cloud

--- dist/routes/hotels.routes.js
+++ dist/routes/hotels.routes.js
@@ -1,28 +1,12 @@
 "use strict";
-// import express from "express";
-// import {
-//   countByCity,
-//   countByType,
-//   createHotel,
-//   deleteHotel,
-//   getHotel,
-//   getHotelRooms,
-//   getHotels,
-//   updateHotel,
-// } from "../controllers/hotel.controller";
-// import { verifyAdmin } from "../middleware/auth.middleware.js";
-// const router = express.Router();
-// //CREATE
-// router.post("/", verifyAdmin, createHotel);
-// //UPDATE
-// router.put("/:id", verifyAdmin, updateHotel);
-// //DELETE
-// router.delete("/:id", verifyAdmin, deleteHotel);
-// //GET
-// router.get("/find/:id", getHotel);
-// //GET ALL
-// router.get("/", getHotels);
-// router.get("/countByCity", countByCity);
-// router.get("/countByType", countByType);
-// router.get("/room/:id", getHotelRooms);
-// export default router;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const hotel_controller_1 = require("../controllers/hotel.controller");
+const router = express_1.default.Router();
+router.get("/:id", hotel_controller_1.getHotelById);
+router.get("/", hotel_controller_1.getHotels);
+router.put("/availability", hotel_controller_1.updateRoomAvailability); // Updating room availability
+exports.default = router;
